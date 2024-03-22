169d19c36544e27f9bcaaed3288d0567ff20dd8a
jcoupal <jcoupal@keepsec.ca>
2024-03-22 02:48:38 -0400

    Changed from createWebHashHistory to createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import MyProjects from '@/views/MyProjects.vue'
 import MyBlog from '@/views/MyBlog.vue'
@@ -23,7 +23,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
