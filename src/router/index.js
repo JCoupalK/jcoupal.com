f3692f8561aeaa03f53434bc799f0531b5268573
Jordan Coupal <jcoupal@keepsec.ca>
2024-03-22 03:28:51 -0400

    Modified router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import MyProjects from '@/views/MyProjects.vue'
 import MyBlog from '@/views/MyBlog.vue'
@@ -23,7 +23,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory("/"),
+  history: createWebHashHistory(),
   routes
 })
 
