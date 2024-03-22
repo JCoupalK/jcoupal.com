fb301581e83a570a64d41727b32af01f6ea5075f
Jordan Coupal <jcoupal@keepsec.ca>
2024-03-22 03:57:42 -0400

    Modified router

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,5 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 RUN npm run build
-
-# Production stage
 EXPOSE 8080
 CMD [ "http-server", "dist" ]
-
-# FROM nginx:stable-alpine as production-stage
-# COPY --from=build-stage /app/dist /usr/share/nginx/html
-# EXPOSE 80
-# CMD ["nginx", "-g", "daemon off;"]
--- src/router/index.js
+++ src/router/index.js
@@ -3,28 +3,25 @@ import HomePage from '@/views/HomePage.vue'
 import MyProjects from '@/views/MyProjects.vue'
 import MyBlog from '@/views/MyBlog.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'HomePage',
-    component: HomePage
-  },
-  {
-    path: '/projects',
-    name: 'MyProjects',
-    component: MyProjects
-  },
-  {
-    path: '/blog',
-    name: 'MyBlog',
-    component: MyBlog
-  },
-]
-
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/projects',
+      name: 'MyProjects',
+      component: MyProjects
+    },
+    {
+      path: '/blog',
+      name: 'MyBlog',
+      component: MyBlog
+    },
+  ]
 })
 
 export default router
